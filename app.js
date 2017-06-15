6838ff7ebdbb7335cc4cc39ca87082fb816612df
Michelle Ferreirae <mnfmnfm@gmail.com>
2017-06-15 12:26:44 -0700

    multiple files

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
 console.log("js loaded");
 
 class GithubFile {
-  constructor() {
-    this.$files = $('.files');
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.className = filePath.replace("/", "-").replace(".", "-")
+    this.$div = $('<div/>').addClass(this.className).html(`
+      <h2>${this.filePath}</h2>
+      <p></p>
+    `);
+    $('.files').append(this.$div);
   }
   fetchUrl(repoUrl) {
     let repoBits = repoUrl.split('/');
-    return `https://api.github.com/repos/${repoBits[3]}/${repoBits[4]}/contents/README.md`
+    return `https://api.github.com/repos/${repoBits[3]}/${repoBits[4]}/contents/${this.filePath}`
   }
 
   gotData(data) {
-    this.fileTitle = data.path;
+    this.filePath = data.path;
     this.fileContents = window.atob(data.content);
     this.renderSelf();
   }
 
   renderSelf() {
-    this.$files.text(`
-        <div>
-          <h2>${this.fileTitle}</h2>
-          <p>${this.fileContents}</p>
-        </div>
-      `)
+    this.$div.children("h2").text(this.filePath);
+    this.$div.children("p").text(this.fileContents);
   }
 }
 $(document).ready(function() {
-  let model = new GithubFile();
-  let $tb = $('.repo-url')
+  let $tb = $('.repo-url');
+  let $fb = $('.file-paths');
+  let models = [];
+  function getModel(filePath) {
+    let model = models.find((model) => model.filePath == filePath);
+    if (!model) {
+      model = new GithubFile(filePath);
+      models.push(model);
+    }
+    return model;
+  }
   $('.file-finder').on('submit', function(event) {
     event.preventDefault();
-    $.ajax({
-      method: "GET",
-      url: model.fetchUrl($tb.val()),
-      success: model.gotData.bind(model)
-    });
+    let files = $fb.val().split(',');
+    for (let file of files) {
+      let model = getModel(file);
+      $.ajax({
+        method: "GET",
+        url: model.fetchUrl($tb.val()),
+        success: model.gotData.bind(model)
+      });
+    }
   })
 
 });
