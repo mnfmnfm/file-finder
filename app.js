9fd6314aae4070e3a68a4d144435be6769d7773a
Michelle Ferreirae <mnfmnfm@gmail.com>
2017-06-15 11:41:32 -0700

    jquery

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("js loaded");
+
+$(document).ready(function() {
+  $('.file-finder').on('submit', function(event) {
+    event.preventDefault();
+    console.log("submitted")
+  })
+
+});
